8178f3d41bc75cbd4e350384e4bcafc03da86092
mimota1994 <34639374+mimota1994@users.noreply.github.com>
2018-04-26 16:38:18 +0800

    加入if

--- BMI.java
+++ BMI.java
@@ -24,6 +24,20 @@ public class BMI {
 		
 		System.out.println("当前的BMI是:"+bmi);
 		
+		if(bmi<18.4)
+			System.out.println("身体状态是:体重过轻");
+		else if(18.5<=bmi&bmi<24)
+			System.out.println("身体状态是:正常范围");
+		else if(24<=bmi&bmi<27)
+			System.out.println("身体状态是:体重过重");
+		else if(27<=bmi&bmi<30)
+			System.out.println("身体状态是:轻度肥胖");
+		else if(30<=bmi&bmi<35)
+			System.out.println("身体状态是:中度肥胖");
+		else
+			System.out.println("身体状态是:重度肥胖");
+		
+		
 	}
 
 }
